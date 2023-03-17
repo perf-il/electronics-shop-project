a0ef32ac26bd411df0327da1777261b5a0c071fa
Ilia Perfilov <ind.climb@gmail.com>
2023-03-17 15:34:32 +0300

    item.py modified - new methods __repr__ and __str__ were added

--- src/item.py
+++ src/item.py
@@ -61,3 +61,9 @@ class Item:
         """
         self.price *= Item.pay_rate
 
+    def __str__(self):
+        return f'{self.__name}'
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
