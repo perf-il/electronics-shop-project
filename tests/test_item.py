367a37a59f8a9744d11dc65ebdcbb8b3578199e2
Ilia Perfilov <ind.climb@gmail.com>
2023-03-27 16:19:59 +0300

    test_phone.py was created

--- tests/test_item.py
+++ tests/test_item.py
@@ -67,25 +67,3 @@ def test_repr(item_1, item_2):
     assert repr(item_2) == "Item('Test2', 20000, 30)"
 
 
-def test_phone(phone_1, phone_2):
-    assert str(phone_1) == 'iPhone 14'
-    assert str(phone_2) == 'iPhone 12'
-    assert repr(phone_1) == "Phone('iPhone 14', 120000, 5, 2)"
-    assert repr(phone_2) == "Phone('iPhone 12', 100000, 7, 1)"
-
-
-def test_sim(phone_1, phone_2):
-    assert phone_1.number_of_sim == 2
-    assert phone_2.number_of_sim == 1
-    with pytest.raises(Exception):
-        phone_1.number_of_sim = 0
-        phone_1.number_of_sim = 3.2
-
-
-def test_add(item_1, phone_1):
-    assert (item_1 + phone_1) == 25
-    assert (item_1 + item_1) == 40
-    assert (phone_1 + phone_1) == 10
-    with pytest.raises(Exception):
-        1 + phone_1
-        item_1 + 2
